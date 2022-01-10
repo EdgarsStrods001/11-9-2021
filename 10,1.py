6fb3498672f4bd608220780ac7815cf2ac912734
edgars strods <82091817+EdgarsStrods001@users.noreply.github.com>
2022-01-10 13:33:17 +0000

    10.01.

---
+++ 10,1.py
@@ -0,0 +1,4 @@
+
+import requests
+
+lapa=request.get("http://vilanuvidusskola.blogspot.com/")
---
+++ iesk.py
@@ -0,0 +1,5 @@
+
+import requests
+
+lapa = requests.get("http://vilanuvidusskola.blogspot.com/")
+print(lapa)
